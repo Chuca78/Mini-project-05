3a8f56b2e48e5b607b86ffb9e3f378712e127d9e
Chuca <Chuca78@users.noreply.github.com>
2022-10-15 07:56:43 -0400

    removed code from HTML, created through js instead

--- script.js
+++ script.js
@@ -3,36 +3,104 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
+// create the textBlock variable
+var textBlock = $('.row');
+// for loop used to create time, text, and buttons
+for (let i = 9; i <= 17; i ++) {
 
-// todo: fix this function to the correct hours
+// create time blocks
+    var timeBlock = $('<p>');
+     // Assign style
+    timeBlock.addClass('hour');
+     // Assign the number to the data-letter attribute
+    timeBlock.attr('data-letter', [i] );
+    // Display the time
+    timeBlock.text([i] + ":00");
+     // Attach the time element
+    textBlock.append(timeBlock);
+
+// create text blocks
+    var textInput = $('<textarea>');
+    // Assign style 
+    textInput.addClass('textarea style="padding: 20px; line-height: 100%" class="textarea col-9 blockquote text-black" rows="1" onsubmit="false' );
+    // Attach the text element
+    textBlock.append(textInput);
+
+// create save buttons
+    // Create button
+    var saveBtn = $('<button>');
+    // Assign style to the button
+    saveBtn.addClass('saveBtn fas fa-save fa-2x trailing');
+    // Attach the button element
+    textBlock.append(saveBtn);
+}
+
+// todo: tie colors to css for text blocks
 // todo: remove console logs
-// todo: 
 // function to find present hour
 function presentHour() {
     var hour = moment().hour();
     console.log("The current hour is: " + hour);
     console.log(typeof hour)
 
-    var areaTags = document.querySelectorAll("textarea");
-    // conditional to compare current hour to text areas for color changes
+    var areaTags = $("textarea");
+    // conditional to compare present hour to current hour for text areas for color changes
     for (var i = 0; i < areaTags.length; i++) {
         console.log(i+hour);
-        if([i+5] < hour){
+        if([i+9] < hour){
             console.log(typeof i)
             console.log(hour)
-            areaTags[i].setAttribute("style", "background-color:grey; font-size: 30px");
+            areaTags[i].setAttribute("style","background-color: #d3d3d3; font-size: 30px");
         }
-        else if([i+4] < hour){
-            areaTags[i].setAttribute("style", "background-color:red; font-size: 30px");
+        else if([i+8] < hour){
+            areaTags[i].setAttribute("style", "background-color:#ff6961; font-size: 30px");
         }
         else{
-            areaTags[i].setAttribute("style", "background-color:green; font-size: 30px");
+            areaTags[i].setAttribute("style", "background-color:#77dd77; font-size: 30px");
         }
     }   
 }
 presentHour();
 
-// todo: tie save button to local storage
-// save button tied to local storage
+
+// todo: fix this - tie save button to local storage
+// var item9 = document.getElementById("#nine");
+
+// var word = localStorage.getItem("word");
+// console.log(typeof word);
+// console.log(word);
+
+// // item9.textContent = word;
+// console.log(item9);
+
+// var saveBtn = $('.saveBtn');
+// saveBtn.on('click', function () {
+//     alert('saved');
+ 
+//     // item9.textContent = word;
+//       localStorage.setItem("word", word);
+//     //   console.log(word);
+//     }
+//   );
 
 
+
+// var saveBtn = $('.saveBtn');
+// saveBtn.on('click', function () {
+//     alert('saved');
+//     var comment = (document.getElementById("9").value);
+//         console.log(comment);
+//         console.log(typeof comment);
+//         var item = (localStorage.setItem("item1", comment))
+//         console.log(item);
+        // $( window ).on( "load",function() {
+        //     localStorage.getItem("item1",document.getElementById("9").value);
+        // }  );
+    // $(window).on('beforeunload', function() {
+    //     var comment = localStorage.setItem(comment,"item1",document.getElementById("9").value);
+    //     console.log(comment);
+    // });
+    // var comment = localStorage.setItem("item1", document.getElementById("9").value);
+    // console.log(comment);
+    // });
+       
